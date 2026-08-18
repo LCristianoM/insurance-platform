b321e087d96b20adfc5607c0ca8a139e60feabb2
LeandroCristianoMendivelso <leandro.cristiano01@gmail.com>
2026-08-18 10:15:34 -0500

    chore: initalize insurance platform

---
+++ src/main/java/com/leaocrist/insurance/InsurancePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.leaocrist.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsurancePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsurancePlatformApplication.class, args);
+	}
+
+}
