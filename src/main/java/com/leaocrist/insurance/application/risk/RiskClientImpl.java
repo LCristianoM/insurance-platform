86f6f427975d76fb6ee49c1705870baaae4fd2df
LeandroCristianoMendivelso <leandro.cristiano01@gmail.com>
2026-08-31 17:15:34 -0500

    Prepare architecture for microservices

--- src/main/java/com/leaocrist/insurance/application/risk/RiskClientImpl.java
+++ src/main/java/com/leaocrist/insurance/application/risk/RiskClientImpl.java
@@ -1,19 +1,29 @@
 package com.leaocrist.insurance.application.risk;
 
 import com.leaocrist.insurance.infrastructure.persistence.risk.RiskRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 @Service
 public class RiskClientImpl implements RiskClient{
 
-    private final RiskRepository riskRepository;
+    private final RestClient restClient;
 
-    public RiskClientImpl(RiskRepository riskRepository){
-        this.riskRepository = riskRepository;
+    @Value("${risk.service.url}")
+    private String riskServiceUrl;
+
+    public RiskClientImpl(RestClient restClient) {
+        this.restClient = restClient;
     }
 
     @Override
     public boolean existsById(Long riskId) {
-        return riskRepository.existsById(riskId);
+        return Boolean.TRUE.equals(
+                restClient.get()
+                        .uri(riskServiceUrl + "/risks/{id}/exists", riskId)
+                        .retrieve()
+                        .body(Boolean.class)
+        );
     }
 }
