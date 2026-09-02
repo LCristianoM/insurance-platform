e05062b62e6320c84267521e41b04f45ceffb4eb
leaocrist <leandro.cristiano01@gmail.com>
2026-09-02 11:48:59 -0500

    Add RiskServiceUnavailableException

--- src/main/java/com/leaocrist/insurance/application/policy/RiskServiceUnavailableException.java
+++ src/main/java/com/leaocrist/insurance/application/policy/RiskServiceUnavailableException.java
@@ -3,7 +3,7 @@ package com.leaocrist.insurance.application.policy;
 public class RiskServiceUnavailableException extends RuntimeException {
     public RiskServiceUnavailableException(String message) {
         super(
-                "Risk service not unavailable."
+                "Risk service not unavailable!"
         );
     }
 }
